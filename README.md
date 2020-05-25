9099cc1db3c03d4b3744c53f320c3f565a212249
renxiaotu <lenzhifeng@163.com>
2020-05-26 01:24:47 +0800

    retest

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Rct
-Renxiaotu canvas tools
+ [rct](https://jquery.com/) — renxiaotu canvas tools
+==============
+
+插件列表
+--------------
+1. [rxt.rct.read](#rxt.rct.read)
+2. [Core Style Guide](https://contribute.jquery.org/style-guide/js/)
+
+### rxt.rct.read
+--------------
+- getData
+
+参数：
+
+| 参数 | 是否必须 | 默认值 | 说明 |
+| canvas | 是 | 无 | 浏览器画布对象 |
+| sx | 否 | 0 | 开始位置x值 |
+| sy | 否 | 0 | 开始位置y值 |
+| sw | 否 | 0 | 读取宽度 |
+| sh | 否 | 0 | 读取高度 |
+
--- src/rxt.rct.read.js
+++ src/rxt.rct.read.js
@@ -15,7 +15,6 @@
             if(typeof sh === "string")sh=parseInt(sh);
             if(sw===0)sw=canvas.width;
             if(sh===0)sh=canvas.height;
-            console.log([sx, sy, sw, sh]);
             return canvas.getContext('2d').getImageData(sx, sy, sw, sh);
         };
 
@@ -27,7 +26,7 @@
          * @param sh
          * @returns {Uint32Array}
          */
-        this.getData32=function (canvas,sx=0,sy=0,sw=null,sh=null) {return new Uint32Array(this.getData(canvas,sx,sy,sw,sh).data.buffer);};
+        this.getData32=function (canvas,sx=0,sy=0,sw=0,sh=0) {return new Uint32Array(this.getData(canvas,sx,sy,sw,sh).data.buffer);};
 
         /**读取画布指定色值所在坐标
          * @param canvas    画布
@@ -37,26 +36,34 @@
          * @returns {[]}    色值数组
          */
         this.getGridByColor=function (canvas,color=[255,255,255,255,0],step=1,reverse=false) {
-            color=color[2]+color[1]*256+color[0]*65536+color[3]*16777216;
-            let data32= this.getData32(canvas);
+            let data= this.getData(canvas).data;
             let gridList=[];
             let width=canvas.width;
             let height=canvas.height;
             step=Math.max(parseInt(step),1);
+
             if(reverse){
+                color[4]++;
                 for (let x = 0; x < width; x += step) {
                     for (let y = 0; y < height; y += step) {
-                        if(data32[y * width + x]!==color){
-                            gridList.push({x:x, y:y});
-                        }
+                        let i=(y * width + x)*4;
+                        let bool=true;
+                        if(Math.abs(data[i]-color[0])>color[4])bool=false;
+                        if(bool&&Math.abs(data[i+1]-color[1])>color[4])bool=false;
+                        if(bool&&Math.abs(data[i+2]-color[2])>color[4])bool=false;
+                        if(bool&&Math.abs(data[i+3]-color[3])>color[4])bool=false;
+                        if(!bool)gridList.push({x:x, y:y,data:[data[i],data[i+1],data[i+2],data[i+3]]});
                     }
                 }
             }else{
-                for (let y = 0; y < height; y += step) {
-                    for (let x = 0; x < width; x += step) {
-                        if(data32[y*width + x]===color){
-                            gridList.push({x:x, y:y});
-                        }
+                for (let x = 0; x < width; x += step) {
+                    for (let y = 0; y < height; y += step) {
+                        let i=(y * width + x)*4;
+                        if(Math.abs(data[i]-color[0])>color[4])continue;
+                        if(Math.abs(data[i+1]-color[1])>color[4])continue;
+                        if(Math.abs(data[i+2]-color[2])>color[4])continue;
+                        if(Math.abs(data[i+3]-color[3])>color[4])continue;
+                        gridList.push({x:x, y:y,data:[data[i],data[i+1],data[i+2],data[i+3]]});
                     }
                 }
             }
